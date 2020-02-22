c5c6d97aa44cb4d33b5e49d8271d5f42aa4207c6
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-02-22 13:52:53 -0800

    declare children components in atm module

--- src/app/atm/atm.module.ts
+++ src/app/atm/atm.module.ts
@@ -3,8 +3,24 @@ import { CommonModule } from '@angular/common';
 
 import { AtmRoutingModule } from './atm-routing.module'
 
+import { DepositComponent }    from './deposit/deposit.component';
+import { FastcashComponent }  from './fastcash/fastcash.component';
+import { MainComponent }  from './main/main.component';
+import { PinComponent }  from './pin/pin.component';
+import { TransactionsComponent }  from './transactions/transactions.component';
+import { TransferComponent }  from './transfer/transfer.component';
+import { WithdrawComponent }  from './withdraw/withdraw.component';
+
 @NgModule({
-  declarations: [],
+  declarations: [
+    DepositComponent,
+    FastcashComponent,
+    MainComponent,
+    PinComponent,
+    TransactionsComponent,
+    TransferComponent,
+    WithdrawComponent
+  ],
   imports: [
     CommonModule,
     AtmRoutingModule
