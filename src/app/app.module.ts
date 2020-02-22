c934b294f902cf261fde49b4e1b5fe8f1d058094
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-02-22 13:32:36 -0800

    import atm module into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { TellerComponent } from './teller/teller.component';
+
 import { AtmComponent } from './atm/atm.component';
+import { AtmModule }     from './atm/atm.module';
+
 import { StartPageComponent } from './start-page/start-page.component';
 import { ContactComponent } from './contact/contact.component';
 
 import { BankService } from './service/bank.service';
-import { PinComponent } from './atm/pin/pin.component';
-import { FastcashComponent } from './atm/fastcash/fastcash.component';
-import { TransferComponent } from './atm/transfer/transfer.component';
-import { DepositComponent } from './atm/deposit/deposit.component';
-import { WithdrawComponent } from './atm/withdraw/withdraw.component';
-import { TransactionsComponent } from './atm/transactions/transactions.component';
-import { MainComponent } from './atm/main/main.component';
+
+
 
 @NgModule({
   declarations: [
@@ -24,17 +23,11 @@ import { MainComponent } from './atm/main/main.component';
     AtmComponent,
     StartPageComponent,
     ContactComponent,
-    PinComponent,
-    FastcashComponent,
-    TransferComponent,
-    DepositComponent,
-    WithdrawComponent,
-    TransactionsComponent,
-    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AtmModule
   ],
   providers: [BankService],
   bootstrap: [AppComponent]
