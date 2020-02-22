93041af256062fb641020b03b0ee8502d55de780
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-02-22 13:28:20 -0800

    import atm routing

--- src/app/atm/atm.module.ts
+++ src/app/atm/atm.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AtmRoutingModule } from './atm-routing.module'
+
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    AtmRoutingModule
   ]
 })
 export class AtmModule { }
