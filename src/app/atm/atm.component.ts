d020816a04f0dbae7e1002ab91f8064aa2d29e68
KRISTINA DONALDSON <DONALDSONKRISTINAR@YAHOO.COM>
2020-02-29 22:08:56 -0800

    changed current balance checkbox to button. added toggle functionality to current balance display

--- src/app/atm/atm.component.ts
+++ src/app/atm/atm.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AtmComponent implements OnInit {
 
-  constructor() { }
+  public show:boolean = false;
+  public buttonName:any = 'Show';
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  toggle() {
+    this.show = !this.show;
+
+    if(this.show)  
+      this.buttonName = "Hide";
+    else
+      this.buttonName = "Show";
   }
+  
+  constructor() { }
 
 }
